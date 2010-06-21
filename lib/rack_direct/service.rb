917953efd0713af3f2caec472578554c3f4725b8
Brian <brian@jambool.com>
2010-06-21 08:41:32 -0700

    Infer name from directory, if it's not explicitly specified.  Allow passing in environment variables to subprocess.

--- lib/rack_direct/service.rb
+++ lib/rack_direct/service.rb
@@ -14,10 +14,13 @@ module RackDirect
     end
 
     @@services = {}
-    def self.start name, path
+    def self.start path, options = {}
+
+      name = options[:name] || File.split(path).last
+
       unless @@services[name]
 
-        tmppath = generate_rackup_file
+        tmppath = generate_rackup_file options[:env]
 
         # TODO: check path to make sure a Rails app exists there
         print "Starting service rack-direct://#{name}..."
@@ -77,12 +80,21 @@ module RackDirect
 
     private
 
-    def self.generate_rackup_file
+    def self.generate_rackup_file environment
       rackup_file_contents = <<-EOF
 require 'rack_direct/direct_handler'
 Rack::Handler.register('#{RACK_DIRECT_ALIAS}', 'RackDirect::DirectHandler')
 # puts "RackDirect::DirectHandler registered"
+EOF
 
+      if environment && environment.is_a?(Hash)
+        rackup_file_contents += "# Passed in environment:\n"
+        environment.each_pair do |k,v|
+          rackup_file_contents += "ENV[#{k}] = #{v}\n"
+        end
+      end
+
+      rackup_file_contents += <<-EOF
 require "config/environment"
 use Rails::Rack::LogTailer
 use Rails::Rack::Static
